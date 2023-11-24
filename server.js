4158f739c76d52e1cbd5250c4332e25660e6fcc8
Serhi Hats <gatsserv@gmail.com>
2023-11-24 20:20:02 +0200

    add express and body-parser

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-const summ = (a, b) => a + b;
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(summ(10,7));
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
+
+app.listen(PORT, ()=>{
+  console.log(`Server started on PORT:${PORT} and starting at http://localhost:${PORT}`)
+})
